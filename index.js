ff2acdbb95c0b3b075c6b76c9e2ffdafd753529e
Shobith Alva <salva@ebay.com>
2022-11-21 12:29:08 -0800

    Add more gitignore

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ function writeKeys() {
     const organization = prompt('Organization: ');
     const key = prompt('API key: ');
 
+    if (fs.existsSync('openai_key.js')) {
+        fs.renameSync('openai_key.js', 'openai_key.js.bak');
+    }
+
     fs.writeFileSync('openai_key.js', `module.exports = {
     'organization': '${organization}',
     'apiKey': '${key}'
