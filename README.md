5dcb5313f30c417d98c9dc21a32090ab8a0b13c8
Shobith Alva <salva@ebay.com>
2022-11-21 12:22:39 -0800

    make some changes

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# haikubot-cli
+# openai-bot-cli
+
+Quick way to create OpenAI bots and test them out
+
+1. Run `yarn store_keys` first to store your keys into the `openai_keys.js` file
+2. To run existing bots: see `scripts` section in `package.json` and run the bot you want, ex: `yarn haikubot`
+3. To add a bot, make a folder, add the index.js and prompt.txt files and then add it to the `scripts` section in `package.json` similar to the other example bots.
--- index.js
+++ index.js
@@ -1,40 +1,15 @@
 const fs = require('fs');
-const readline = require('readline');
-const { organization, apiKey } = require('./openai_key');
-const { Configuration, OpenAIApi } = require("openai");
+const prompt = require('prompt-sync')();
 
-const configuration = new Configuration({ apiKey });
-const openai = new OpenAIApi(configuration);
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout,
-    terminal: false
-});
+function writeKeys() {
+    const organization = prompt('Organization: ');
+    const key = prompt('API key: ');
 
-function generatePrompt(question) {
-    return `${fs.readFileSync('haikubot_prompt', 'utf8')}Q: ${question}`;
+    fs.writeFileSync('openai_key.js', `module.exports = {
+    'organization': '${organization}',
+    'apiKey': '${key}'
+}
+`);
 }
 
-async function answer(question) {
-    const completion = await openai.createCompletion({
-        model: "text-davinci-002",
-        prompt: generatePrompt(question),
-        temperature: 0.9,
-        max_tokens: 400
-    });
-    return {
-        result: completion.data.choices[0].text
-    };
-}
-
-const haiku_repl = function () {
-    rl.question('>>> ', async (prompt) => {
-        await answer(prompt)
-            .then(response => response.result.length > 0 ? response.result : '🤷🏽')
-            .then(result => result.replace(/A:[\n]?/, '\n').trim())
-            .then(console.log);
-        haiku_repl();
-    });
-};
-
-haiku_repl();
+writeKeys();
